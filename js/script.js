834b84c255c268e5b892c1fd43d69c2b104478f7
Dayronaguilera <aguilera.dayron@gmail.com>
2021-05-30 21:50:19 +0200

    Bonus age

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
 
 // dati cliente km
-var x = prompt("Inserire numero km di percorrenza");
-// dati cliente età
-var y = prompt("Inserire la vostra eta");
+var x = prompt("Inserire km di percorrenza");
 
-var price = parseInt(x) * 0.21;
+// dati cliente età
+var y = prompt("Inserire l'anno di nascita");
+var age = parseInt(y) - 2021;
+// prezzo piu sconti 
+var price = x * 0.21;
 var scontoMinori = price - price /100 * 20;
 var scontoSenior = price - price /100 * 40;
 
-if (y < 18) {
+// comandi da eseguire
+if (age < 18) {
     document.getElementById("result").innerHTML = scontoMinori + " € ";
-} else { 
-    if (y >= 65) {
+} 
+else { 
+    if (age >= 65) {
         document.getElementById("result").innerHTML = scontoSenior + " € ";
-    } else {
+    } 
+    else {
         document.getElementById("result").innerHTML = price + " € ";
     }
 } 
